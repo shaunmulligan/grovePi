73855d9e1031d5fd8a3825c4a0fe085cb4757108
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-01-20 13:44:11 +0000

    add sensors

--- firmware_version.py
+++ firmware_version.py
@@ -2,7 +2,17 @@ import grovepi
 
 try:
     print "GrovePi has firmware version:", grovepi.version()
+    # Connect the Grove Temperature & Humidity Sensor Pro to digital port D4
+	# SIG,NC,VCC,GND
+	sensor = 4
 
+	while True:
+	    try:
+	        [temp,humidity] = grovepi.dht(sensor,1)
+	        print "temp =", temp, " humidity =", humidity
+
+	    except IOError:
+	        print "Error"
 except KeyboardInterrupt:
     print "KeyboardInterrupt"
 except IOError:
--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+udevd &
+#modprobe bcm2708_wdog
 modprobe i2c-bcm2708
 modprobe i2c-dev
 
