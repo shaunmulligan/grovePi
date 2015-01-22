1f207210de0146cae66f3fb201bcb1d6cc506cc6
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-01-22 12:09:46 +0000

    added print statement

--- firmware_version.py
+++ firmware_version.py
@@ -1,5 +1,17 @@
 import time
 import grovepi
+from grove_oled import *
+
+oled_init()
+oled_clearDisplay()
+oled_setNormalDisplay()
+oled_setVerticalMode()
+time.sleep(.1)
+
+print "hello world to screen"
+for i in range(0,12):
+    oled_setTextXY(i,0)
+    oled_putString("Hello World")
 
 # Connect the Grove Temperature & Humidity Sensor Pro to digital port D4
 # SIG,NC,VCC,GND
