277f1b391c077e57df202380890f55c16c2c2b56
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-01-22 15:26:31 +0000

    added some comments

--- firmware_version.py
+++ firmware_version.py
@@ -2,6 +2,7 @@ import time
 import grovepi
 from grove_oled import *
 
+# Connect the OLED module to port I2C-1
 oled_init()
 oled_clearDisplay()
 oled_setNormalDisplay()
@@ -18,11 +19,9 @@ while True:
         [temp,humidity] = grovepi.dht(sensor,1)
         print "temp =", temp, " humidity =", humidity
         oled_setTextXY(0,0)
-        tempStr = "Temp= "+ str(temp)+"C"
-    	oled_putString(tempStr)
+    	oled_putString("Temp= "+ str(temp)+"C")
     	oled_setTextXY(1,0)
-    	humStr = "Hum= "+ str(humidity)
-    	oled_putString(humStr)
+    	oled_putString("Hum= "+ str(humidity))
         time.sleep(30)
     except IOError:
         print "Error"
